b3f9c84478dd92c13d87094989c6fa8a692a879d
Rohit Thimaya <RThimaya@bclc.com>
2024-11-16 17:05:21 -0800

    setup tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <h1>Habit Tracker App</h1>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
     </>
   )
 }
